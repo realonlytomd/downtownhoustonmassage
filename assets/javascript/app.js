cad5c407a7bd7518e342c8dad58a6471b7026959
realonlytomd <mclaughlintomd@gmail.com>
2018-05-07 12:20:47 -0500

    js hides and shows id's

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
     console.log("hello");
     
+    $("#myCarousel").hide();
+    $("#googleMap").hide();
+    $("#viewSpace").hide();
+
+    $(document).on("click", "#mySpace", function() {
+
+        $("#logo").hide();
+        $("#myCarousel").show();
+        $("#viewSpace").hide();
+        $("#googleMap").hide();
+    });
+
+    $(document).on("click", "#myMassage", function() {
+
+        $("#logo").hide();
+        $("#myCarousel").hide();
+        $("#viewSpace").show();
+        $("#googleMap").hide();
+    });
+
+    $(document).on("click", "#myLocation", function() {
+
+        $("#logo").hide();
+        $("#myCarousel").hide();
+        $("#viewSpace").hide();
+        $("#googleMap").show();
+    });
+
 });
