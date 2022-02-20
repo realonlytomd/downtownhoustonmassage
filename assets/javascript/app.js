0bc367551fe056a7a13fc10c2f35d255f1785631
Tom McLaughlin <mclaughlintomd@gmail.com>
2022-02-19 18:25:14 -0600

    removed log with former path

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -143,7 +143,7 @@ $(document).ready(function() {
     // Create Firebase "watcher". Responds when a new input has been made (child)
 	database.ref().on("child_added", function(snapshot) {
         //print value of snapshot to console
-        console.log("child added shapshot of firebase data (val): ", snapshot.val());
+        //console.log("child added shapshot of firebase data (val): ", snapshot.val());
         console.log("shapshot.val().users.meUid.blogDate: ", snapshot.val().users.meUid.blogDate);
         var newEntry = $("<div>");
         var newDate = $("<h5>").text(snapshot.val().users.meUid.blogDate);
