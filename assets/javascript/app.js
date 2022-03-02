d035a4548d298048a5e1857cd1cf4eec999e9f5a
Tom McLaughlin <mclaughlintomd@gmail.com>
2022-03-01 19:46:35 -0600

    more logs in signme out

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -61,11 +61,13 @@ $(document).ready(function() {
     // sign me out
     $(document).on("click", "#signMeOut", function(event) {
         event.preventDefault();
+        console.log("inside sign me out");
         firebase.auth().signOut()
             .then(() => {
             // Sign-out successful.
             console.log("I've signed out");
-            }).catch((error) => {
+            })
+            .catch((error) => {
                 console.log("error: ", error);
             // An error happened.
           });
