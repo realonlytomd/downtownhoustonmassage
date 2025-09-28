1111b8cf66b04bd5718f8c9f242700f6db892008
realonlytomd <mclaughlintomd@gmail.com>
2025-09-28 14:08:39 -0500

    adjusting blog div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -258,7 +258,7 @@ $(document).ready(function() {
         $("#massageInfo").hide();
         $("#googleMap").hide();
         $("#blog").show();
-        $("#review").show();
+        $("#review").hide();
         $(".padNumber").show();
         //console.log("background color: " + $("footer").css("background-color"));
         if($("footer").css("background-color") === "rgb(30, 54, 99)") {
